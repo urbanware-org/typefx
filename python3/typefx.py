99dca186a136fddbbb6265320998d4ecf179b819
Ralf Kilian <github@urbanware.org>
2019-03-24 15:31:20 +0100

    Raise version number

--- python3/typefx.py
+++ python3/typefx.py
@@ -3,7 +3,7 @@
 
 # ============================================================================
 # TypeFX - Typewriter effect text printer
-# Copyright (C) 2018 by Ralf Kilian
+# Copyright (C) 2019 by Ralf Kilian
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 #
 # GitHub: https://github.com/urbanware-org/typefx
