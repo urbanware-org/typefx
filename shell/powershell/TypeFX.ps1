30d43675e0db5a628dbb4e8f73d74ff6312c29d2
urbanware-org <github@urbanware.org>
2019-08-11 21:32:17 +0200

    Add dynamic output effect

--- shell/powershell/TypeFX.ps1
+++ shell/powershell/TypeFX.ps1
@@ -8,7 +8,20 @@
 # GitLab: https://gitlab.com/urbanware-org/typefx
 # ============================================================================
 
-$TypeFxVersion = "1.0.0"
+$TypeFxVersion = "1.1.0"
+
+Function Type-Dynamic {
+    [Int32]$DelayMin = $args[0]
+    [Int32]$DelayMax = $args[1]
+    [String]$String = $args[2]
+
+    For ($i = 0; $i -lt $String.Length; $i++) {
+        Write-Host $String[$i] -NoNewLine
+        $Delay = Get-Random -Minimum $DelayMin -Maximum $DelayMax
+        Start-Sleep -Milliseconds $Delay
+    }
+    Write-Host
+}
 
 Function Type-Static {
     [Int32]$Delay = $args[0]
@@ -18,4 +31,5 @@ Function Type-Static {
         Write-Host $String[$i] -NoNewLine
         Start-Sleep -Milliseconds $Delay
     }
+    Write-Host
 }
