f3f9f36eb988b495548a480eab11beb454d19366
urbanware-org <github@urbanware.org>
2022-07-05 22:33:22 +0200

    Add keystroke sound version

---
+++ sound/README.md
@@ -0,0 +1,88 @@
+# *TypeFX* with keystroke sounds
+
+**Table of contents**
+*   [Definition](#definition)
+*   [Details](#details)
+*   [Usage](#usage)
+
+----
+
+## Definition
+
+This directory contains an **experimental** version of the *TypeFX* module, which, unlike the original, additionally plays keystroke sounds while typing.
+
+I haven't done much with sound modules in *Python* (or at all) before, so this was a first attempt. Since then, I haven't had the time or leisure to play around with these sound modules either. As mentioned above, this was an experiment, so it is uncertain whether I will enhance or modify it at all.
+
+[Top](#)
+
+## Details
+
+So far, the script supports the three sound modules *playsound*, *pydub* and *simpleaudio*.
+
+However, there were some issues with *pydub* (including *ffmpeg* crashes), which have not been investigated further and it is recommended to use one of the others instead.
+
+Depending on the sound module you are using, the script may crash or does not play the sounds properly. Therefore, you should first try out which module works best for you.
+
+The script path contains the sub-directory `sounds`. Inside this directory there are five files:
+
+*   `key_static.wav` (used for all characters dor static "machine like" mode)
+*   `key_space.wav` (used for spaces for dynamic "typewriter like" mode, only)
+*   `key_stroke1.wav` (one of the random sounds used for dynamic "typewriter like" mode, only)
+*   `key_stroke2.wav` (one of the random sounds used for dynamic "typewriter like" mode, only)
+*   `key_stroke3.wav` (one of the random sounds used for dynamic "typewriter like" mode, only)
+
+The files `key_static.wav` and `key_space.wav` are required.
+
+For the keystrokes at least `key_stroke1.wav` must exist, `key_stroke2.wav` and `key_stroke3.wav` are optional. However, it is recommended to provide all of them. Further keystroke sounds e.g. `key_stroke4.wav` are not supported, so there is a maximum of three sounds.
+
+[Top](#)
+
+## Usage
+
+The function call itself is identical to that of the usual *TypeFX* methods, but with an additional parameter for the preferred sound module.
+
+### Default behavior
+
+If no specific sound module is given, the script will use *playsound* by default.
+
+```python
+typefx.static(string, delay=10)
+```
+```python
+typefx.dynamic(string, delay_min=1, delay_max=100)
+```
+
+### Module *playsound*
+
+Even though this module is the default one, it can also be given explictily.
+
+```python
+typefx.static(string, delay=10, sound_module="playsound")
+```
+```python
+typefx.dynamic(string, delay_min=1, delay_max=100, sound_module="playsound")
+```
+
+### Module *pydub*
+
+Simply change (or add) the corresponding `sound_module` parameter:
+
+```python
+typefx.static(string, delay=10, sound_module="pydub")
+```
+```python
+typefx.dynamic(string, delay_min=1, delay_max=100, sound_module="pydub")
+```
+
+### Module *simpleaudio*
+
+Simply change (or add) the corresponding `sound_module` parameter:
+
+```python
+typefx.static(string, delay=10, sound_module="simpleaudio")
+```
+```python
+typefx.dynamic(string, delay_min=1, delay_max=100, sound_module="simpleaudio")
+```
+
+[Top](#)
---
+++ sound/python3/typefx_sound.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# TypeFX - Typewriter effect text printer
+# Experimental keystroke sound module
+# Copyright (C) 2022 by Ralf Kilian
+# Distributed under the MIT License (https://opensource.org/licenses/MIT)
+#
+# GitHub: https://github.com/urbanware-org/typefx
+# GitLab: https://gitlab.com/urbanware-org/typefx
+#
+
+__version__ = "1.1.4"   # with keystroke sound effects
+
+#
+# As already mentioned above, this is an experimental version of the TypeFX
+# module, which, unlike the original, additionally plays keystroke sounds
+# while typing. For further information and usage examples see:
+#
+#     https://github.com/urbanware-org/typefx/sound/README.md
+#
+
+import os
+import random
+import sys
+import time
+
+try:
+    import playsound as ps
+    playsound_installed = True
+except:
+    playsound_installed = False
+
+try:
+    from pydub import AudioSegment as pydub_seg
+    from pydub.playback import play as pydub_play
+    pydub_installed = True
+except:
+    pydub_installed = False
+
+try:
+    import simpleaudio as sa
+    simpleaudio_installed = True
+except:
+    simpleaudio_installed = False
+
+if not playsound_installed and not pydub_installed and \
+   not simpleaudio_installed:
+    raise Exception(
+        "No supported sound module ('playsound', 'pydub' or "
+        "'simpleaudio') found.")
+
+dir_sounds = \
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "sounds")
+
+
+def keystroke_sound(char="", static=True, sound_module="playsound"):
+    if char == "":
+        raise Exception("Keystroke must not be empty.")
+    if sound_module == "":
+        raise Exception("The sound module must not be empty.")
+
+    if not os.path.exists(os.path.join(dir_sounds, "key_static.wav")):
+        raise Exception(
+            "Required sound file 'key_static.wav' is missing.")
+    elif not os.path.exists(os.path.join(dir_sounds, "key_space.wav")):
+        raise Exception(
+            "Required sound file 'key_space.wav' is missing.")
+    elif not os.path.exists(os.path.join(dir_sounds, "key_stroke1.wav")):
+        raise Exception(
+            "Required sound file 'key_stroke1.wav' is missing.")
+
+    key_sound = ""
+    if static:
+        key_sound = os.path.join(dir_sounds, "key_static.wav")
+    else:
+        if char == " ":
+            key_sound = os.path.join(dir_sounds, "key_space.wav")
+        else:
+            while not os.path.exists(key_sound):
+                random.seed()
+                key_random = random.randrange(1, 4)
+                key_sound = \
+                    os.path.join(
+                        dir_sounds, "key_stroke" + str(key_random) + ".wav")
+
+    if sound_module == "playsound":
+        if not playsound_installed:
+            raise Exception("The given sound module is not installed.")
+        ps.playsound(key_sound, block=False)
+    elif sound_module == "pydub":
+        if not pydub_installed:
+            raise Exception("The given sound module is not installed.")
+        seg = pydub_seg.from_wav(key_sound)
+        pydub_play(seg)
+    elif sound_module == "simpleaudio":
+        wave_object = sa.WaveObject.from_wave_file(key_sound)
+        wave_object.play()
+    else:
+        raise Exception("The given sound module is not supported.")
+
+
+def dynamic(string, delay_min=1, delay_max=100, sound_module="playsound"):
+    """
+        Print the input text "typewriter like" with a dynamic delay (in
+        milliseconds).
+    """
+    if string == "":
+        return
+
+    try:
+        delay_min = int(delay_min)
+    except:
+        raise Exception("The minimum delay must be an integer.")
+
+    try:
+        delay_max = int(delay_max)
+    except:
+        raise Exception("The maximum delay must be an integer.")
+
+    if delay_max <= delay_min:
+        raise Exception("The maximum delay must be greater than the "
+                        "minimum delay.")
+    if delay_min <= 0:
+        raise Exception("The minimum delay must be greater than zero.")
+    if delay_max <= 0:
+        raise Exception("The maximum delay must be greater than zero.")
+
+    for char in string:
+        random.seed()
+        wait = float(random.randrange(delay_min, delay_max)) / 1000
+        keystroke_sound(char, False, sound_module)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(wait)
+
+
+def static(string, delay=10, sound_module="playsound"):
+    """
+        Print the input text "machine like" with a static delay (in
+        milliseconds).
+    """
+    if string == "":
+        return
+
+    try:
+        delay = int(delay)
+        wait = float(delay) / 1000
+    except:
+        raise Exception("The delay must be an integer.")
+
+    if delay <= 0:
+        raise Exception("The delay must be greater than zero.")
+
+    delay = int(delay)
+    wait = float(delay) / 1000
+    for char in string:
+        keystroke_sound(char, True, sound_module)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(wait)
+
+# EOF
